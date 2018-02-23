9251ddb42c25122be85128f7eb070c37a3cb8f48
MaMo-ben <34936430+MaMo-ben@users.noreply.github.com>
2018-02-23 10:47:15 +0100

    Create 2017Bacs.sh

---
+++ 2017Bacs.sh
@@ -0,0 +1,345 @@
+#!/bin/bash
+echo -n $'\E[31m'
+echo $''
+echo $'      _,.'
+echo $'    ,` -.)'
+echo $'   \'( _/\'-\\\\-.'
+echo $'  /,|`--._,-^|          ,'
+echo $'  \\_| |`-._/||          ,\'|'
+echo $'    |  `-, / |         /  /'
+echo $'    |     || |        /  /'
+echo $'     `r-._||/   __   /  /'
+echo $' __,-<_     )`-/  `./  /'
+echo $'\'  \\   `---\'   \\   /  /'
+echo $'    |           |./  /'
+echo $'    /           //  /'
+echo $'\\_/\' \\         |/  /'
+echo $' |    |   _,^-\'/  /'
+echo $' |    , ``  (\\/  /_'
+echo $'  \\,.->._    \\X-=/^'
+echo $'  (  /   `-._//^`'
+echo $'   `Y-.____(__}'
+echo $'    |     {__)'
+echo $'          ()`'
+echo whats your name 
+read $name
+echo Hello $name This script download 2017 2016 2015 2014 2013 2012 2011 2010 BACS
+echo Its made by mamoun Benhafsa 
+echo 
+mkdir 2017
+echo
+cd 2017
+echo
+mkdir hisory-geographie
+echo
+mkdir isllamique
+echo
+mkdir arabic
+echo
+mkdir math
+echo
+mkdir phisique
+echo
+mkdir sience
+echo
+mkdir english
+echo
+mkdir french
+############################################################################################################################################histry download#
+echo
+cd hisory-geographie 
+echo
+mkdir exam 
+echo
+mkdir solitions
+echo
+cd exam
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/hisgeo-semtm-bac2017-page-1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/hisgeo-semtm-bac2017-page-2_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/hisgeo-semtm-bac2017-page-3_orig.jpg
+echo	
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/hisgeo-semtm-bac2017-page-4_orig.jpg
+echo
+cd ..
+echo
+cd solitions 
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-051_orig.jpg 
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-052_orig.jpg	
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-053_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-054_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-055_orig.jpg
+####################################################################################################################################################end_history###
+echo
+cd ..
+echo
+cd ..
+###########################################################################################################################################isslamique_download#####
+echo
+cd isllamique 
+echo
+mkdir exam
+echo
+mkdir solitions
+echo
+cd exam
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/islamic-bac2017-page-1_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/islamic-bac2017-page-2_orig.jpg
+echo
+cd ..
+echo
+cd solitions
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-083_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-084_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-085_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-086_orig.jpg
+##########################################################################################################################################end_isslamique###############
+echo
+cd ..
+echo
+cd ..
+##############################################################################################################################################arabic_download#########
+echo
+cd arabic
+echo
+mkdir exam
+echo
+mkdir solitions
+echo
+cd exam
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/arabic-sci-bac2017-page-1_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/arabic-sci-bac2017-page-2_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/arabic-sci-bac2017-page-3_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/arabic-sci-bac2017-page-4_orig.jpg
+echo
+cd ..
+echo
+cd solitions
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-060_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-061_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-062_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-063_orig.jpg
+#########################################################################################################################################end_arabic#####
+echo
+cd ..
+echo
+cd ..
+######################################################################################################################################download _math####
+echo
+cd math
+echo
+mkdir exam
+echo
+mkdir solitions
+echo
+cd exam
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-page-1_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-page-2_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-page-3_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-page-4_orig.jpg
+echo
+cd ..
+echo
+cd solitions 
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-1_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-2_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-3_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-4_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-5_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/math-se-bac2017-correction-page-6_orig.jpg
+##########################################################################################################################################end_math#############
+echo
+cd ..
+echo
+cd ..
+###############################################################################################################################download_phisique###############
+echo
+cd phisique
+echo
+mkdir exam
+echo
+mkdir solitions
+echo
+cd exam
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-2_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-3_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-4_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-5_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-6_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-7_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/physics-se-bac2017-page-8_orig.jpg
+echo
+cd ..
+echo 
+cd solitions
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-038_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-039_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-040_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-041_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-042_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-043_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-044_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-045_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-046_orig.jpg
+#######################################################################################################"end_phisique"#####
+echo
+cd ..
+echo
+cd ..
+##########################################################sience_download######################################################
+echo
+cd sience
+echo 
+mkdir exam
+echo 
+mkdir solitions
+echo 
+cd exam 
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-2_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-3_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-4_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-5_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-6_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-7_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-page-8_orig.jpg
+echo 
+cd ..
+echo 
+cd solitions
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-2_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-3_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-4_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-5_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-6_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-7_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/sciences-se-bac2017-correction-page-8_orig.jpg
+#########################################################################################end_sience################################
+echo 
+cd ..
+echo
+cd ..
+###################################################################################################english_download##################
+echo 
+cd english
+echo 
+mkdir exam
+echo 
+mkdir solitions
+echo
+cd exam
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-ens01_orig.jpg
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-ens02_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-ens03_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-ens04_orig.jpg
+echo
+cd ..
+echo
+cd solitions
+echo 
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-079_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-080_orig.jpg
+##############################################################################################################end_english################
+echo	
+cd ..
+echo	
+cd ..
+####################################################################################################################french_download############
+echo
+cd french
+echo
+mkdir exam
+echo 
+mkdir solitions
+echo
+cd exam
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/french-sci-bac2017-page-1_1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/french-sci-bac2017-page-2_1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/french-sci-bac2017-page-3_1_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/french-sci-bac2017-page-4_1_orig.jpg
+echo
+cd ..
+echo
+cd solitions
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-072_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-073_orig.jpg
+echo
+wget https://www.ency-education.com/uploads/3/0/9/3/309326/bac2017-all-corrections-page-074_orig.jpg
+##############################################################################################################
